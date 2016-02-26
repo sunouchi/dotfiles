58e27781a24680826163398af78b7bff16b01822
簾内貴就 <sunouchitakanari@BITA01062.local>
2016-02-26 11:22:51 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# make directory
+mkdir $HOME/.vim/tmp
+mkdir $HOME/.vim/tmp/backup
+mkdir $HOME/.vim/tmp/swap
+mkdir $HOME/.vim/tmp/undo
+mkdir $HOME/.vim/tmp/info
+mkdir $HOME/.vim/tmp/info/viminfo
+
+# symbolic links
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -s $HOME/dotfiles/.tmux $HOME/.tmux
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
