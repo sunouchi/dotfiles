e511c1e14a71839d3052ec27665694a78b2385aa
sunouchi <t@sunouchi.jp>
2017-05-23 12:33:04 +0900

    update .emacs.d setup

--- .emacs.d/setup.sh
+++ .emacs.d/setup.sh
@@ -1,4 +1,11 @@
-# dotfilesへのエイリアスをホームディレクトリに作成
-ln -s ~/dotfiles/.emacs.d/elisp ~/.emacs.d/elisp
-ln -s ~/dotfiles/.emacs.d/conf ~/.emacs.d/conf
-ln -s ~/dotfiles/.emacs.d/public_repos ~/.emacs.d/public_repos
+#!/bin/sh
+EMACS_DIR=$HOME/dotfiles/.emacs.d/
+
+# 各ディレクトリを作成する
+dirs=('elisp' 'conf' 'public_repos')
+for dir in ${dirs[@]}; do
+    d=$EMACS_DIR$dir
+    if [ ! -e $d ]; then
+	mkdir -p $d
+    fi
+done
