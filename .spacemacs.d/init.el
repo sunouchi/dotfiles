e63654920fa6e6a24f86007c842c78cda601bedf
sunouchi <t@sunouchi.jp>
2017-06-29 17:13:26 +0900

    update key bind

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -392,6 +392,6 @@ you should place your code here."
   (or (looking-at "[0-9]+")
       (error "No number at point"))
   (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
-(global-set-key (kbd "C-'") 'increment-number-at-point)
+(global-set-key (kbd "C-+") 'increment-number-at-point)
 (global-set-key (kbd "C--") 'decrement-number-at-point)
 
