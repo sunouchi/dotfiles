e6f1250f062986117736490569399d35f5defa61
sunouchi <t@sunouchi.jp>
2018-04-02 12:35:01 +0900

    .tmux ディレクトリは個別マシンごとに作成する

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkdir $HOME/.vim/tmp/info/viminfo
 # symbolic links
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
-ln -s $HOME/dotfiles/.tmux $HOME/.tmux
+#ln -s $HOME/dotfiles/.tmux $HOME/.tmux
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
